2cac8039ab850e4d98f7a8ea70cf177cf6acc99d
sproodlev <inky-04-tensor@icloud.com>
2024-11-15 15:58:26 +0000

    Add Colors

---
+++ lib/presentation/shared/style/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+abstract class CtColors {
+  static const Color darkBlue = Color(0xFF151D51);
+  static Color darkBlue70 = Color(0xFF151D51).withOpacity(0.7);
+  static Color darkBlue20 = Color(0xFF151D51).withOpacity(0.2);
+  static Color darkBlue10 = Color(0xFF151D51).withOpacity(0.1);
+
+  static const Color green = Color(0xFF5CD6C0);
+  static Color green10 = Color(0xFF5CD6C0).withOpacity(0.1);
+
+  static const Color red = Color(0xFFED5F59);
+  static Color red10 = Color(0xFFED5F59).withOpacity(0.1);
+
+  static const Color lightBlue = Color(0xFF70C3FF);
+  static const Color purpleBlue = Color(0xFF4B65FF);
+}
