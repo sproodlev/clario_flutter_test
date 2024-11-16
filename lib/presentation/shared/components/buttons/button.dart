4f8f4a34c035944beb86ee1d15c801b3e10758a5
sproodlev <inky-04-tensor@icloud.com>
2024-11-16 21:54:22 +0000

    Create button

---
+++ lib/presentation/shared/components/buttons/button.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertest/presentation/shared/style/colors.dart';
+import 'package:fluttertest/presentation/shared/style/text_styles.dart';
+
+class CtButton extends StatelessWidget {
+  final double width;
+  final void Function() onPressed;
+  final String text;
+
+  const CtButton({
+    required this.width,
+    required this.onPressed,
+    required this.text,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => onPressed(),
+      child: Container(
+        height: 48.h,
+        width: width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0.r),
+          gradient: LinearGradient(
+            colors: [CtColors.lightBlue, CtColors.purpleBlue],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            transform: GradientRotation(30 * pi / 180),
+          ),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: CtTextStyles.s16w700white,
+          ),
+        ),
+      ),
+    );
+  }
+}
